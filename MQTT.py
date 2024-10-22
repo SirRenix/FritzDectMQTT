0cbd6b1700d049ff485ed653e809d452e2470b6d
SirRenix <7069603+SirRenix@users.noreply.github.com>
2024-10-22 10:44:48 +0200

    Refactored MQTT class for cleaner structure and added message receiving capability
    #Refactored the MQTT class to improve readability and maintainability:
    - Moved MQTT client initialization into the constructor (__init__) to streamline the setup process.
    - Separated connection logic into connect() method to handle the actual connection flow more clearly.
    
    #Implemented receiveData() method:
    - Now capable of receiving and processing incoming MQTT messages.
    - Added JSON payload decoding with error handling and logging for invalid or incomplete messages.
    - Integrated an action handler system to process messages based on action and data fields in the JSON payload.
    
    - Enhanced logging to provide better visibility into the MQTT connection lifecycle and message processing.
    
    -Updated on_connect() method:
    -Subscribes to a default topic (home/devices/MyFritzBox/#) after successful connection.
    
    #Added authentication: MQTT client now sets a username and password if provided in the secrets configuration.
    
    #General improvements:
    - Improved error handling during reconnection attempts with exponential backoff.
    - Refined logging messages for both sending and receiving MQTT messages.

--- MQTT.py
+++ MQTT.py
@@ -1,106 +1,119 @@
-
 import os
 import logging.config
-import json, yaml
+import json
+import yaml
 import time
 import paho.mqtt.client as mqttClient
 
 class MQTT:
-    """
-
-    """
-
-    MQTTClient: mqttClient
-
-    # -------------------
     def __init__(self, ConfigData: dict = None, SecretData: dict = None):
-        """
-        """
         assert (ConfigData is not None), "No config data given"
         assert (SecretData is not None), "No secret data given"
 
         self.mqttConfData = ConfigData["MQTT"]
-        self.mqttSecData = SecretData["MQTT_BROKER"]["RASPI_PH"]
-        self.fritzbox: str = ConfigData["QUERY"]["FB"]
+        self.mqttSecData = SecretData["MQTT_BROKER"]["RASPI"]
+        self.fritzbox = ConfigData["QUERY"]["FB"]
 
         self.logger = logging.getLogger(__name__)
 
-    # -------------------
-    def connect(self) -> mqttClient:
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                print("Connected to MQTT Broker ")
-            else:
-                print("Failed to connect to MQTT Broker, return code %d\n".format(rc))
+        self.MQTTClient = mqttClient.Client(client_id=self.mqttConfData["clientId"])
 
-        def on_disconnect(client, userdata, rc):
-            FIRST_RECONNECT_DELAY = 1
-            RECONNECT_RATE = 2
-            MAX_RECONNECT_COUNT = 12
-            MAX_RECONNECT_DELAY = 60
+        if self.mqttSecData["user"] and self.mqttSecData["password"]:
+            self.MQTTClient.username_pw_set(self.mqttSecData["user"], self.mqttSecData["password"])
 
-            self.logger.warning("Disconnected with result code: %s", rc)
-            reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
-            while reconnect_count < MAX_RECONNECT_COUNT:
-                self.logger.warning("Reconnecting in %d seconds...", reconnect_delay)
-                time.sleep(reconnect_delay)
+        self.MQTTClient.on_connect = self.on_connect
+        self.MQTTClient.on_disconnect = self.on_disconnect
+        self.MQTTClient.on_message = self.receiveData
+        self.MQTTClient.enable_logger(self.logger)
 
-                try:
-                    client.reconnect()
-                    self.logger.info("Reconnected successfully!")
-                    return
-                except Exception as myErr:
-                    self.logger.error("%s. Reconnect failed. Retrying...", myErr)
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            self.logger.info("Connected to MQTT Broker")
+            client.subscribe("home/devices/MyFritzBox/#")
+        else:
+            self.logger.error(f"Failed to connect to MQTT Broker, return code {rc}")
 
-                reconnect_delay *= RECONNECT_RATE
-                reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
-                reconnect_count += 1
-            self.logger.warning("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+    def on_disconnect(self, client, userdata, rc):
+        FIRST_RECONNECT_DELAY = 1
+        RECONNECT_RATE = 2
+        MAX_RECONNECT_COUNT = 12
+        MAX_RECONNECT_DELAY = 60
 
+        self.logger.warning("Disconnected with result code: %s", rc)
+        reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+        while reconnect_count < MAX_RECONNECT_COUNT:
+            self.logger.warning("Reconnecting in %d seconds...", reconnect_delay)
+            time.sleep(reconnect_delay)
 
-        self.MQTTClient = mqttClient.Client(mqttClient.CallbackAPIVersion.VERSION1, self.mqttConfData["clientId"])
-#        if self.secretData["user"] is not None and self.secretData["password"] is not None:
-#            self.MQTTClient.username_pw_set(self.secretData["user"], self.secretData["password"])
+            try:
+                client.reconnect()
+                self.logger.info("Reconnected successfully!")
+                return
+            except Exception as myErr:
+                self.logger.error(f"{myErr}. Reconnect failed. Retrying...")
 
-        self.MQTTClient.on_connect = on_connect
-        self.MQTTClient.on_disconnect = on_disconnect
+            reconnect_delay *= RECONNECT_RATE
+            reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+            reconnect_count += 1
 
+        self.logger.warning("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+    def connect(self):
         try:
             retCode = self.MQTTClient.connect(str(self.mqttSecData["ip"]), int(self.mqttSecData["port"]))
-
-            if mqttClient.MQTTErrorCode.MQTT_ERR_SUCCESS == retCode:
-                self.logger.info("Connection successfull")
+            if retCode == 0:
+                self.logger.info("Connection successful")
             else:
-                self.logger.error("Connect Broker {}:{} not successfull (retCode='{}'), no exception given"
-                              .format(self.mqttSecData["ip"], self.mqttSecData["port"], retCode))
+                self.logger.error(f"Failed to connect to broker {self.mqttSecData['ip']}:{self.mqttSecData['port']} (retCode={retCode})")
         except Exception as err:
-            self.logger.error("Connection error: {}".format(err.args))
-
+            self.logger.error(f"Connection error: {err}")
         return self.MQTTClient
 
-    # -----------------
     def sendData(self, addTopic: str = "", sendData: dict = None):
         assert (sendData is not None), "No data to send given"
 
         currentTopic = self.mqttConfData["maintoken"] + "/" + self.fritzbox + "/" + addTopic
 
-        self.logger.debug("Current topic: '{}'".format(currentTopic))
+        self.logger.debug(f"Current topic: '{currentTopic}'")
 
         sendString = json.dumps(sendData, ensure_ascii=False)
         result = self.MQTTClient.publish(currentTopic, sendString)
 
         if result[0] == 0:
-            self.logger.info("Send '{}' to {}:{} with topic '{}'".format(sendString, self.mqttSecData["ip"],
-                                                                         self.mqttSecData["port"],
-                                                                         currentTopic))
+            self.logger.info(f"Sent '{sendString}' to {self.mqttSecData['ip']}:{self.mqttSecData['port']} with topic '{currentTopic}'")
         else:
-            self.logger.error("Failed to send message to topic '{}'".format(currentTopic))
+            self.logger.error(f"Failed to send message to topic '{currentTopic}'")
 
+    def receiveData(self, client, userdata, message):
+        raw_payload = message.payload.decode("utf-8")
+        print(f"topic {message.topic}: Raw received payload: {raw_payload}")
+        self.logger.debug(f"Received raw payload: {raw_payload}")
+    
+        #self.logger.debug(f"Received message on topic {message.topic}: {message.payload.decode('utf-8')}")
+        #print(f"Received message on topic {message.topic}: {message.payload.decode('utf-8')}")
 
-# ======================================
+        try:
+            payload = json.loads(raw_payload)
+            action_type = payload.get("action", None)
+            data = payload.get("data", None)
+
+            if not action_type or data is None:
+                self.logger.error(f"Invalid message received: {message.payload}")
+                return
+
+            if hasattr(self, 'action_handler'):
+                self.action_handler(action_type, data)
+            else:
+                self.logger.warning("No action handler defined in MQTT class.")
+
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Failed to decode JSON: {e}")
+        except Exception as e:
+            self.logger.error(f"Error processing message: {e}")
+
 if __name__ == '__main__':
     CONFIG_FILE_NAME_YAML = "configdata.cfg"
-    SECRETS_FILE_NAME_YAML= "secrets.yaml"
+    SECRETS_FILE_NAME_YAML = "secrets.yaml"
 
     if not os.path.exists(CONFIG_FILE_NAME_YAML):
         raise NameError(f"Config file '{CONFIG_FILE_NAME_YAML}' is not accessible.")
@@ -113,34 +126,33 @@ if __name__ == '__main__':
         secrets = yaml.safe_load(f.read())
 
     if "logging" not in configuration:
-        raise Exception("No logging configuration in configuration file '{}' available.".format(CONFIG_FILE_NAME_YAML))
+        raise Exception(f"No logging configuration in configuration file '{CONFIG_FILE_NAME_YAML}' available.")
 
     logging.config.dictConfig(configuration["logging"])
     logger = logging.getLogger("MQTT")
     logger.info("------------Start MQTT Test ------------")
-    logger.info("Used Configfile: '{}'".format(CONFIG_FILE_NAME_YAML))
+    logger.info(f"Used Configfile: '{CONFIG_FILE_NAME_YAML}'")
+
+    # Beispielhafte Verwendung:
+    mqtt = MQTT(configuration, secrets)
+    mqtt.connect()
 
     testdata = {
-       "116570433098" : {
-           "AIN": "116570433098",
-           "name": "Au\u00dfenStkd. Garage: Wasserpumpe",
-           "temp": 6.5,
-           "power": 0.0
-       },
-       "116570387991" : {
-          "AIN": "116570387991",
-          "name": "Leuchtb\u00e4umchen vor Haust\u00fcr",
-          "temp": 13.5,
-          "power": 4.28
-       }
+        "116570433098": {
+            "AIN": "116570433098",
+            "name": "AußenSteckdose Garage: Wasserpumpe",
+            "temp": 6.5,
+            "power": 0.0
+        },
+        "116570387991": {
+            "AIN": "116570387991",
+            "name": "Leuchtbäumchen vor Haustür",
+            "temp": 13.5,
+            "power": 4.28
+        }
     }
 
-#    mqtt = MQTT(logger, configuration, secrets, "FB_Moehlau")
-#    mqtt.connect()
-
     for ain in testdata.keys():
-#        mqtt.sendData(addTopic=ain, sendData=testdata[ain])
-        print("name: {}".format(testdata[ain]["name"]))
-        print(testdata[ain]["name"])
-
-
+        mqtt.sendData(addTopic=ain, sendData=testdata[ain])
+        print(f"name: {testdata[ain]['name']}")
+        print(testdata[ain]['name'])
